7333f43090e9c2429a3ea9413fde63e65be9dfa7
Michael Berkowitz <michael.berkowitz@gmail.com>
2025-06-10 22:54:36 -0400

    Fix default opts

--- lib/oscill_ex.ex
+++ lib/oscill_ex.ex
@@ -3,7 +3,7 @@ defmodule OscillEx do
   Documentation for `OscillEx`.
   """
 
-  def start_server(opts) do
+  def start_server(opts \\ []) do
     Supervisor.start_child(OscillEx.Supervisor, OscillEx.Server.child_spec(opts))
   end
 end
