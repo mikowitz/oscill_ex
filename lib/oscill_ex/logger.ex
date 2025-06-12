e1ba888902a6e2bd6eb3299751af2844703dc5d4
Michael berkowitz <michael.berkowitz@gmail.com>
2025-06-12 19:35:26 -0400

    Cleanup logger

--- lib/oscill_ex/logger.ex
+++ lib/oscill_ex/logger.ex
@@ -6,11 +6,7 @@ defmodule OscillEx.Logger do
   def server_starting(command), do: Logger.info("Server starting with `#{command}`")
   def server_started(command), do: Logger.info("Server started with `#{command}`")
   def server_start_failed(command), do: Logger.error("Could not start server with `#{command}`")
-  def server_stopped(reason), do: Logger.warning("scsynth server stopped with #{inspect(reason)}")
   def server_quit, do: Logger.info("`scsynth` server quit")
   def missing_executable(path), do: Logger.error("Could not find executable `#{path}`")
-  def unexpected_message(msg), do: Logger.debug("Unexpected message: #{String.trim(msg)}")
-  def info(msg), do: Logger.info(String.trim(msg))
-  def debug(msg), do: Logger.debug(String.trim(msg))
-  def udp(msg), do: Logger.info(inspect(msg))
+  def udp(msg), do: Logger.info(inspect(to_string(msg)))
 end
