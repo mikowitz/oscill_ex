52d0895417edd2d76cfe0465ba3c62f2b8ba03b9
Michael Berkowitz <michael.berkowitz@gmail.com>
2025-06-18 19:07:31 -0400

    mix new

---
+++ lib/oscill_ex.ex
@@ -0,0 +1,18 @@
+defmodule OscillEx do
+  @moduledoc """
+  Documentation for `OscillEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OscillEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
