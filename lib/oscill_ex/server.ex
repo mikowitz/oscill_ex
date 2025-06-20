3a4685e76d557b4f24de123b149780864dd2152c
Michael Berkowitz <michael.berkowitz@gmail.com>
2025-06-19 23:27:56 -0400

    server and scsynth process lifecycle

---
+++ lib/oscill_ex/server.ex
@@ -0,0 +1,178 @@
+defmodule OscillEx.Server do
+  use GenServer
+
+  @type state :: %{
+          status: :stopped | :booting | :error | :crash,
+          error: term() | nil,
+          port: port() | nil,
+          monitor: reference() | nil,
+          config: %{
+            executable: String.t(),
+            args: [String.t()]
+          }
+        }
+
+  @default_config %{
+    executable: "scsynth",
+    args: ["-u", "57110", "-R", "0", "-l", "1"]
+  }
+
+  #########
+  ## API ##
+  #########
+
+  def start_link(opts \\ %{}) do
+    GenServer.start_link(__MODULE__, Enum.into(opts, %{}))
+  end
+
+  def boot(pid) do
+    GenServer.call(pid, :boot)
+  end
+
+  def quit(pid) do
+    GenServer.call(pid, :quit)
+  end
+
+  ###############
+  ## CALLBACKS ##
+  ###############
+
+  @impl GenServer
+  def init(opts \\ %{}) do
+    config = Map.merge(@default_config, opts)
+
+    {:ok,
+     %{
+       status: :stopped,
+       error: nil,
+       config: config,
+       port: nil,
+       monitor: nil
+     }}
+  end
+
+  @impl GenServer
+  def handle_call(:boot, _, %{status: :booting} = state) do
+    {:reply, {:error, :already_running}, state}
+  end
+
+  def handle_call(:boot, _, state) do
+    %{config: %{executable: executable, args: args}} = state
+
+    {resp, new_state} =
+      case validate_executable(executable) do
+        :ok ->
+          port =
+            Port.open(
+              {:spawn_executable, "./bin/wrapper"},
+              [
+                {:args, [executable | args]},
+                :binary,
+                :exit_status
+              ]
+            )
+
+          monitor = Port.monitor(port)
+
+          {:ok, %{state | status: :booting, port: port, monitor: monitor}}
+
+        {:error, error} ->
+          {
+            {:error, error},
+            %{state | status: :error, error: error}
+          }
+      end
+
+    {:reply, resp, new_state}
+  end
+
+  def handle_call(:quit, _, %{status: :stopped} = state) do
+    {:reply, :ok, state}
+  end
+
+  def handle_call(:quit, _, %{port: port} = state) when is_port(port) do
+    Port.demonitor(state.monitor, [:flush])
+    Port.close(port)
+
+    {:reply, :ok, %{state | port: nil, monitor: nil, status: :stopped}}
+  end
+
+  @impl GenServer
+  def handle_info({port, {:exit_status, exit_code}}, %{port: port} = state) do
+    Port.demonitor(state.monitor, [:flush])
+
+    case exit_code do
+      0 ->
+        {:noreply, %{state | port: nil, monitor: nil, status: :stopped}}
+
+      _ ->
+        {:noreply,
+         %{state | port: nil, monitor: nil, status: :crashed, error: {:exit, exit_code}}}
+    end
+  end
+
+  def handle_info({:DOWN, _, :port, port, reason}, %{port: port} = state) do
+    Port.demonitor(state.monitor, [:flush])
+    {:noreply, %{state | port: nil, monitor: nil, status: :crashed, error: {:exit, reason}}}
+  end
+
+  def handle_info({port, {:data, data}}, %{port: port} = state) do
+    cond do
+      Regex.match?(~r/ERROR.*address in use/, data) ->
+        Port.demonitor(state.monitor, [:flush])
+
+        {:noreply,
+         %{
+           state
+           | port: nil,
+             monitor: nil,
+             status: :crashed,
+             error: {:exit, :scsynth_port_in_use}
+         }}
+
+      Regex.match?(~r/ERROR.*Invalid option/, data) ||
+          Regex.match?(~r/ERROR.*There must be a -u/, data) ->
+        Port.demonitor(state.monitor, [:flush])
+
+        {:noreply,
+         %{
+           state
+           | port: nil,
+             monitor: nil,
+             status: :crashed,
+             error: {:exit, :scsynth_invalid_args}
+         }}
+
+      true ->
+        {:noreply, state}
+    end
+  end
+
+  def handle_info(_info, state) do
+    {:noreply, state}
+  end
+
+  defp validate_executable(executable) do
+    case File.stat(executable) do
+      {:ok, %File.Stat{type: :regular}} ->
+        if can_execute?(executable) do
+          :ok
+        else
+          {:error, {:permission_denied, executable}}
+        end
+
+      {:ok, %File.Stat{type: type}} ->
+        {:error, {:not_executable, "#{executable} is a #{type}, not an executable file"}}
+
+      {:error, :enoent} ->
+        {:error, {:file_not_found, executable}}
+    end
+  end
+
+  defp can_execute?(executable) do
+    case System.cmd("test", ["-x", executable]) do
+      {_, 0} -> true
+      _ -> false
+    end
+  end
+end
